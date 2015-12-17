bbd2260733abbe93d47fe4f52da24da2f965b205
Nicoletti Sébastien <s.nico@live.fr>
2015-12-17 22:16:55 +0100

    Démarrage: Création de l'architecture

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xmlEesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xmlEesApp
+ */
+angular.module('xmlEesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xmlEesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xmlEesApp
+ */
+angular.module('xmlEesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
