5c87b2b421d6c2b779634f563b4d00b9caab8223
Nicoletti Sébastien <s.nico@live.fr>
2015-12-18 19:52:53 +0100

    Server_client: Liens html et redirections
    - Lien vers les fichiers statiques ( images, js, css )
    - Lien vers les page html ( protocol get )

--- server_client.js
+++ server_client.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var app = express();
- 
-app.use(express.static(__dirname + '/bower_components'));
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+
+//Static files
+app.use("/bower_components", express.static(__dirname + '/bower_components'));
+app.use("/images", express.static(__dirname + '/app/images'));
+app.use("/scripts", express.static(__dirname + '/app/scripts'));
+app.use("/styles", express.static(__dirname + '/app/styles'));
+
+//web pages
+app.get(['/', 'index.html'], function (req, res) {
+  res.sendFile("app/index.html", {root: __dirname });
+});
+
+app.get('/views/:name', function (req, res) {
+  res.sendFile("app/views/" + req.params.name, {root: __dirname });
+});
  
 app.listen(3000);
