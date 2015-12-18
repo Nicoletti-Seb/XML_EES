180300f4e7be63e4aefda966525a1634c99c46ae
Nicoletti Sébastien <s.nico@live.fr>
2015-12-18 20:49:43 +0100

    Server_client: gestion du favicon

--- server_client.js
+++ server_client.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
+
+
 
 //Static files
 app.use("/bower_components", express.static(__dirname + '/bower_components'));
 app.use("/images", express.static(__dirname + '/app/images'));
 app.use("/scripts", express.static(__dirname + '/app/scripts'));
 app.use("/styles", express.static(__dirname + '/app/styles'));
+app.use(favicon(__dirname + '/app/favicon.ico')); 
 
 //web pages
 app.get(['/', 'index.html'], function (req, res) {
