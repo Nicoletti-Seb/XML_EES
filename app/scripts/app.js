d8bf03dcc337e2682695a55dd5dc70d1f25af410
Nicoletti Sébastien <s.nico@live.fr>
2016-01-01 16:42:01 +0100

    Statistics: Ajout de la page statistiques dans la structure.
    - Ajout d'un controleur statistics
    - Ajout du chemin dans le root provider

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,6 +24,11 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
+      .when('/statistics', {
+        templateUrl: 'views/statistics.html',
+        controller: 'StatisticsCtrl',
+        controllerAs: 'statistics'
+      })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl',
---
+++ app/scripts/controllers/statistics.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xmlEesApp.controller:StatisticsCtrl
+ * @description
+ * # StatisticsCtrl
+ * Controller of the xmlEesApp
+ */
+angular.module('xmlEesApp')
+  .controller('StatisticsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
