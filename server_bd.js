5d397d9712f8eced0f3f9651d9452d5b014d1423
leopaul29 <leopaulmartin@hotmail.fr>
2016-01-09 23:45:36 +0100

    server_bd:add method getAcademies

--- server_bd.js
+++ server_bd.js
@@ -26,7 +26,7 @@ exports.getTypes = function(callBack) {
 	});
 }
 
-// retourne la liste de nom des tutelle d'etablissement
+// retourne la liste de nom des tutelles d'etablissement
 exports.getTutelles = function(callBack) {
 	s.query("declare option output:method 'json';<json type='object'>   <types type='array'>{   for $x in distinct-values(//etablissement/tutelle)  return <_ type='object'>{ <nom type='string'>{string($x)}</nom>}</_>}</types> </json>").execute(function (err, result) {
 	    if (err) {
@@ -37,9 +37,20 @@ exports.getTutelles = function(callBack) {
 	});
 }
 
-// retourne la liste de nom des statut d'etablissement
+// retourne la liste de nom des statuts d'etablissement
 exports.getStatuts = function(callBack) {
 	s.query("declare option output:method 'json';<json type='object'>   <types type='array'>{   for $x in distinct-values(//etablissement/statut)  return <_ type='object'>{ <nom type='string'>{string($x)}</nom>}</_>}</types> </json>").execute(function (err, result) {
+	    if (err) {
+	    	callBack(err);
+	    } else {
+	    	callBack(result.result);
+	    }
+	});
+}
+
+// retourne la liste de nom des academies
+exports.getAcademies = function(callBack) {
+	s.query("declare option output:method 'json';<json type='object'>   <types type='array'>{   for $x in distinct-values(//etablissement/academie)  return <_ type='object'>{ <nom type='string'>{string($x)}</nom>}</_>}</types> </json>").execute(function (err, result) {
 	    if (err) {
 	    	callBack(err);
 	    } else {
