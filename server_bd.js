9a49663afa002aa69e76a112397247ad3ea80514
leopaul29 <leopaulmartin@hotmail.fr>
2016-01-09 23:48:36 +0100

    server_bd:add method getUniversites

--- server_bd.js
+++ server_bd.js
@@ -58,3 +58,14 @@ exports.getAcademies = function(callBack) {
 	    }
 	});
 }
+
+// retourne la liste de nom des academies
+exports.getUniversites = function(callBack) {
+	s.query("declare option output:method 'json';<json type='object'>   <types type='array'>{   for $x in distinct-values(//etablissement/universite)  return <_ type='object'>{ <nom type='string'>{string($x)}</nom>}</_>}</types> </json>").execute(function (err, result) {
+	    if (err) {
+	    	callBack(err);
+	    } else {
+	    	callBack(result.result);
+	    }
+	});
+}
