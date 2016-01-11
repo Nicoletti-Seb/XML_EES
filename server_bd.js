1ddac8e29acca4d4d1050dde86a1b4cd22a96a60
leopaul29 <leopaulmartin@hotmail.fr>
2016-01-11 16:37:15 +0100

    server_bd:modification methode getEtablissementPositions (att en plus: adresse et lien)

--- server_bd.js
+++ server_bd.js
@@ -6,7 +6,7 @@ s.execute('open etablissement_superieur');
 
 // retourne la position géographique d'un etablissement
 exports.getEtablissementPositions = function(callBack) {
-	s.query("declare option output:method 'json'; <json type='object'>   <etabs type='array'>{   for $x in //etablissement     return <_ type='object'>{           <nom type='string'>{string($x/nom)}</nom> |           <x type='string'>{string($x/longitude_X)}</x> |           <y type='string'>{string($x/latitude_Y)}</y>         }         </_> }   </etabs> </json>").execute(function (err, result) {
+	s.query("declare option output:method 'json'; <json type='object'>   <etabs type='array'>{   for $x in //etablissement     return <_ type='object'>{           <nom type='string'>{string($x/nom)}</nom> |           <x type='string'>{string($x/longitude_X)}</x> |           <y type='string'>{string($x/latitude_Y)}</y>    | <adresse type='string'>{string($x/adresse)}</adresse> | <lien type='string'>{string($x/lien)}</lien>    }         </_> }   </etabs> </json>").execute(function (err, result) {
 	    if (err) {
 	    	callBack(err);
 	    } else {
