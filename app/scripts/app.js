e52ef35ff2fd3c69b963b63a1ebe5d67edc39a09
Aurore dechamps <dechamps.aurore@gmail.com>
2016-01-12 11:41:58 +0100

    debug carte

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,7 +23,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'views/index.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
