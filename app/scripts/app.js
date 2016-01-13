0c8d95a1761171edaed7e70dd6cdc853b133df00
Nicoletti Sébastien <s.nico@live.fr>
2016-01-13 19:03:01 +0100

    Rechercher: Ajout du menu rechercher
    + correction de l'architecture

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,11 +19,22 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ])
-  .config(function ($routeProvider) {
+  ]).controller('MainCtrl',function( $scope, $http, $location ){
+
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.changeView = function(path){
+      $location.path(path);
+    }
+
+  }).config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'views/map.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
@@ -31,6 +42,11 @@ angular
         templateUrl: 'views/statistics.html',
         controller: 'StatisticsCtrl',
         controllerAs: 'statistics'
+      })
+      .when('/search', {
+        templateUrl: 'views/search.html',
+        controller: 'SearchCtrl',
+        controllerAs: 'search'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
@@ -40,4 +56,66 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  }).directive('myMap',function () {
+      // directive link function
+      var link = function(scope, element, attrs) {
+          var map, infoWindow;
+          var markers = [];
+          
+          // map config
+          var mapOptions = {
+              center: new google.maps.LatLng(50, 2),
+              zoom: 4,
+              mapTypeId: google.maps.MapTypeId.ROADMAP,
+              scrollwheel: false
+          };
+          
+          // init the map
+          function initMap() {
+              if (map === void 0) {
+                  map = new google.maps.Map(element[0], mapOptions);
+              }
+          }    
+          
+          // place a marker
+          function setMarker(map, position, title, content) {
+              var marker;
+              var markerOptions = {
+                  position: position,
+                  map: map,
+                  title: title,
+                  icon: ' http://www.google.com/mapfiles/marker.png'
+              };
+
+              marker = new google.maps.Marker(markerOptions);
+              markers.push(marker); // add marker to array
+              
+              google.maps.event.addListener(marker, 'click', function () {
+                  // close window if not undefined
+                  if (infoWindow !== void 0) {
+                      infoWindow.close();
+                  }
+                  // create new window
+                  var infoWindowOptions = {
+                      content: content
+                  };
+                  infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+                  infoWindow.open(map, marker);
+              });
+          }
+          
+          // show the map and place some markers
+          initMap();
+          
+          setMarker(map, new google.maps.LatLng(51.508515, -0.125487), 'London', 'Just some content');
+          setMarker(map, new google.maps.LatLng(52.370216, 4.895168), 'Amsterdam', 'More content');
+          setMarker(map, new google.maps.LatLng(48.856614, 2.352222), 'Paris', 'Text here');
+      };
+      
+      return {
+          restrict: 'A',
+          template: '<div id="map_canvas"></div>',
+          replace: true,
+          link: link
+      };
   });
