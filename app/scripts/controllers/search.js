d8cb43beecda8d4581d0aca8dd5be691b30e4616
Nicoletti Sébastien <s.nico@live.fr>
2016-01-13 23:59:07 +0100

    Rechercher: Ajout du formulaire

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -8,13 +8,44 @@
  * Controller of the xmlEesApp
  */
 angular.module('xmlEesApp')
-  .controller('SearchCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+  .controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
+    $scope.data = {
+    	type:null,
+    	tutelle:null,
+    	statut:null,
+    	academie:null,
+    	universite:null
+   };
+
+
+    $http.post("http://localhost:3000/getTypes").success(function(data) {
+          $scope.types = data["types"];
+        });
+
+    $http.post("http://localhost:3000/getTutelles").success(function(data) {
+          $scope.tutelles = data["types"];
+        });
+
+    $http.post("http://localhost:3000/getStatuts").success(function(data) {
+          $scope.statuts = data["types"];
+        });
+
+
+    $http.post("http://localhost:3000/getAcademies").success(function(data) {
+          $scope.academies = data["types"];
+        });
+
+    $http.post("http://localhost:3000/getUniversites").success(function(data) {
+          $scope.universites = data["types"];
+        });
     
+
+
 
   }]);
